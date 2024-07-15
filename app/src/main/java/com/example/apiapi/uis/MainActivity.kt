03bded8518f80514d5b0a67ff53eaab98c9308e4
RITESHKUMAR7488 <riteshman30@gmail.com>
2024-07-15 18:25:42 +0530

    jjj

--- app/src/main/java/com/example/apiapi/uis/MainActivity.kt
+++ app/src/main/java/com/example/apiapi/uis/MainActivity.kt
@@ -5,47 +5,51 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.apiapi.MyApplication
 import com.example.apiapi.R
 import com.example.apiapi.adapters.RecipeAdapter
 import com.example.apiapi.apis.ApiInterface
 import com.example.apiapi.databinding.ActivityMainBinding
 import com.example.apiapi.models.Recipe
+import com.example.apiapi.repositories.MyRepository
+import com.example.apiapi.viewModel.MyViewModel
+import com.example.apiapi.viewModelFactory.MyViewModelFactory
 import retrofit2.Callback
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MyViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         with(binding) {
-            val retrofitBuilder = Retrofit.Builder()
-                .baseUrl("https://dummyjson.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(ApiInterface::class.java)
+            // ye code retrofit call karne ke liye hai
 
-            val retrofitData = retrofitBuilder.getRecipes()
-
-            retrofitData.enqueue(object : Callback<Recipe?> {
-                override fun onResponse(
-                    p0: retrofit2.Call<Recipe?>,
-                    p1: retrofit2.Response<Recipe?>
-                ) {
-                    val list = p1.body()?.recipes
-                    val adapter = list?.let { RecipeAdapter(it, this@MainActivity) }
-                    recyclerView.adapter = adapter
-
-                }
+            val application = application as MyApplication
+            val retrofitBuilder = application.retrofit
+            val apiInterface = retrofitBuilder.create(ApiInterface::class.java)
 
 
-                override fun onFailure(p0: retrofit2.Call<Recipe?>, p1: Throwable) {
-                    Toast.makeText(this@MainActivity, "p1.message", Toast.LENGTH_SHORT).show()
-                }
+            val repository = MyRepository(apiInterface)
+            viewModel = ViewModelProvider(
+                this@MainActivity,
+                MyViewModelFactory(repository)
+            )[MyViewModel::class.java]
 
-            })
+            viewModel.getRecipes().observe(this@MainActivity) {
+
+                val list = it.recipes
+
+                val adapter = RecipeAdapter(list, this@MainActivity)
+                recyclerView.adapter = adapter
+
+            }
+
 
 
         }
